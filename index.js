1f3da4ca95f296562e2265833dd2cc2c465ba723
Shahadat Hossain <60577101+shahadathossain4536@users.noreply.github.com>
2023-05-04 07:39:13 +0600

    kk

--- index.js
+++ index.js
@@ -19,9 +19,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const serviceCollection = client.db("car-doctor").collection("service");
+    const serviceCollection = client.db("car-doctor").collection("services");
+    const orderCollection = client.db("car-doctor").collection("orders");
 
-    app.get("/service", async (req, res) => {
+    app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const service = await cursor.toArray();
@@ -29,11 +30,34 @@ async function run() {
     });
 
     app.get("/service/:id", async (req, res) => {
+      // console.log(req.params);
       const id = req.params.id;
-      console.log("id", id);
-      const query = { _id: ObjectId(id) };
+      // console.log("id", id);
+      const query = { _id: new ObjectId(id) };
+      // console.log(query);
       const result = await serviceCollection.findOne(query);
+      // console.log(result);
+
       res.send(result);
+    });
+
+    // order
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+    app.get("/orders", async (req, res) => {
+      console.log(req.query);
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
     });
   } catch (error) {}
 }
